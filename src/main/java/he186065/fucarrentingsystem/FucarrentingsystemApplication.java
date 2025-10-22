f2c84eb730f803b5efd081bdb864328c590e1508
longmoon2k4 <longmoon2004@gmail.com>
2025-10-22 21:03:12 +0700

    first commit

---
+++ src/main/java/he186065/fucarrentingsystem/FucarrentingsystemApplication.java
@@ -0,0 +1,13 @@
+package he186065.fucarrentingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FucarrentingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FucarrentingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/he186065/fucarrentingsystem/FucarrentingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package he186065.fucarrentingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FucarrentingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
